8a5791507c28fa376f8a14bb9295dcd3ee7fc283
Babin Dmitrij <d.babin@cft.ru>
2023-07-18 16:49:01 +0700

    main ☄ fix recaptcha

---
+++ .eslintrc.js
@@ -0,0 +1,92 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier'
+  ],
+  env: {
+    browser: true,
+    es2021: true
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['tsconfig.json']
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@next/eslint-plugin-next'],
+  rules: {
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', disallowTypeAnnotations: false }
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
+    'react/button-has-type': 0,
+    'react/no-unstable-nested-components': [
+      2,
+      {
+        allowAsProps: true
+      }
+    ],
+    'react/no-array-index-key': 0,
+    'no-param-reassign': 0,
+    'no-restricted-syntax': 0,
+    '@typescript-eslint/no-loop-func': 0,
+    'sort-keys': [1, 'asc', { natural: true, allowLineSeparatedGroups: true }],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-sort-props': [
+      2,
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        multiline: 'last',
+        ignoreCase: true,
+        reservedFirst: true
+      }
+    ],
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+    'import/export': 0,
+    'import/order': [
+      2,
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal'
+          }
+        ],
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ]
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    tabWidth: 2,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: 'always',
+    useTabs: false,
+    printWidth: 100,
+    trailingComma: 'none',
+    semi: true
+};
