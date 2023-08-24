50176b89485cf5e22190501fd0b18d8739c0fe22
j-0-n-e-z <j000neeez@gmail.com>
2023-08-24 22:50:10 +0500

    change edges to nodes

--- app/profile/[login]/@repositories/page.tsx
+++ app/profile/[login]/@repositories/page.tsx
@@ -10,13 +10,13 @@ interface RepositoriesProps {
 const Repositories = async ({ params }: RepositoriesProps) => {
   const repositories = await gql.Repositories({ login: params.login });
 
-  if (!repositories.user?.repositories.edges?.length) return null;
+  if (!repositories.user?.repositories.nodes?.length) return null;
 
   return (
     <>
       <Text size='lg'>Repositories</Text>
       <Flex direction='column' gap='sm' w='100%'>
-        {repositories.user?.repositories.edges?.map(({ node }) => {
+        {repositories.user.repositories.nodes.map((node) => {
           if (!node) return null;
           return <RepositoryItem key={node.id} login={params.login} repository={node} />;
         })}
